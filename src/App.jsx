bb56ceb795c67607eba6a4a5f875714ef7938443
kushal acharya <kushalacharya544@gmail.com>
2026-02-21 12:54:41 +0545

    Initial version: Create and display todos

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim() === "") return;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <h1>Todo CRUD App</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter todo"
+      />
+
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
